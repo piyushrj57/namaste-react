854149bc974bcff394e2fb834927de42688a8cc7
PEEYUSH RAJ <peeyushraj@PEEYUSHs-MacBook-Air.local>
2026-04-28 02:00:51 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-const parent = React.createElement("div", {id: "parent"}, 
-    React.createElement("h1", {id: "child"}, "Hello I am a H6 tag")
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = <h1 id="heading">Hello World from JSX.</h1>;
 
 
+//React Functional Component
+const Title = () => {
+    return <h1 className="head">Namaste React usingJSX </h1>
+}
+const HeadingComponent = () =>
+(<div>
+    <Title />
+     <h1 className="heading">Namaste React </h1>
+</div>);
 
-
-
+//Root
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+//Render
+root.render(<HeadingComponent />);
