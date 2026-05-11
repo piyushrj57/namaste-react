f03d12881156a00058739d1a139991a7fb9d3c26
PEEYUSH RAJ <piyushrj57@gmail.com>
2026-05-12 02:48:32 +0530

    Episode 4

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id="heading">Hello World from JSX.</h1>;
-
-
 //React Functional Component.
-const Title = () => {
-    return <h1 className="head">Namaste React usingJSX </h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/thumbnails/021/953/308/small/food-ordering-app-logo-with-points-and-fork-shapes-in-the-center-free-vector.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const Body =() =>{
+    return(
+        <div className="body">
+           <div className="search"> Search</div>
+        </div>
+    )
 }
-const HeadingComponent = () =>
-(<div>
-    <Title />
-     <h1 className="heading">Namaste React </h1>
-</div>);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 //Root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //Render
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
