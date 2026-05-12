2825fd62f81e2f940645c3e63b13a47dd92aa93a
PEEYUSH RAJ <piyushrj57@gmail.com>
2026-05-13 01:01:16 +0530

    Episode 4

--- App.js
+++ App.js
@@ -3,39 +3,67 @@ import ReactDOM from "react-dom/client";
 
 //React Functional Component.
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://static.vecteezy.com/system/resources/thumbnails/021/953/308/small/food-ordering-app-logo-with-points-and-fork-shapes-in-the-center-free-vector.jpg"
-          alt="logo"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="https://static.vecteezy.com/system/resources/thumbnails/021/953/308/small/food-ordering-app-logo-with-points-and-fork-shapes-in-the-center-free-vector.jpg"
+                    alt="logo"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
-const Body =() =>{
-    return(
+
+const Restaurantcard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/5c5effc9-fae6-447d-850a-39f269c93084_10583.JPG" alt="logo" />
+            <h3>Meghna Foods</h3>
+            <h4>Biryani, Asian, North Indian</h4>
+            <h5>Rating: 4.5</h5>
+        </div>
+    )
+}
+const Body = () => {
+    return (
         <div className="body">
-           <div className="search"> Search</div>
+            <div className="search"> Search</div>
+            <div className="res-container">
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+            <Restaurantcard />
+</div>
         </div>
     )
 }
 const AppLayout = () => {
-  return (
-    <div className="app">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    );
 };
 
 //Root
